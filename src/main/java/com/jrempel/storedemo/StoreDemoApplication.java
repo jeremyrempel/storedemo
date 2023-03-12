4e7fc261846e3591bcc620b8bbc8b0640673b3ce
Jeremy Rempel <jeremyrempel@gmail.com>
2023-03-12 19:42:10 -0400

    initial commit

---
+++ src/main/java/com/jrempel/storedemo/StoreDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jrempel.storedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreDemoApplication.class, args);
+	}
+
+}
